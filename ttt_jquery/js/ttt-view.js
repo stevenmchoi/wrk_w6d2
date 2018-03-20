6b80ab6d751650efb4b82047cc3f87f1092063de
Steven Choi <steven.m.choi@gmail.com>
2018-03-20 15:30:45 -0700

    Complete bindEvents

--- ttt_jquery/js/ttt-view.js
+++ ttt_jquery/js/ttt-view.js
@@ -1,11 +1,21 @@
 class View {
   constructor(game, $el) {
+    this.game = game;
     this.$el = $el;
-    this.setupBoard();
 
+    this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", $(".tile"), (event) => {
+      let $tile = $(event.target);
+      let tilePos = $tile.attr("id");
+      const pos = tilePos.split("-").slice(1);
+      console.log(pos);
+      this.game.playMove(pos);
+    });
+  }
 
   makeMove($square) {}
 
@@ -14,9 +24,12 @@ class View {
     const $grid = $("<ul></ul>").addClass("grid");
     this.$el.append($grid);
 
-    for (let idx = 0; idx < 9; idx++) {
-      const $tile = $("<li></li>").addClass(`tile tile-${idx}`);
-      $grid.append($tile);
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const $tile = $("<li></li>").addClass(`tile`);
+        $tile.attr("id", `tile-${row}-${col}`);
+        $grid.append($tile);
+      }
     }
   }
 }
