9f32441d38d2ad3a4a3b4218b8961bc9affa1a3a
Steven Choi <steven.m.choi@gmail.com>
2018-03-20 14:44:49 -0700

    Update board setup

--- ttt_jquery/js/ttt-view.js
+++ ttt_jquery/js/ttt-view.js
@@ -1,11 +1,24 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.$el = $el;
+    this.setupBoard();
+
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+
+    const $grid = $("<ul></ul>").addClass("grid");
+    this.$el.append($grid);
+
+    for (let idx = 0; idx < 9; idx++) {
+      const $tile = $("<li></li>").addClass(`tile tile-${idx}`);
+      $grid.append($tile);
+    }
+  }
 }
 
 module.exports = View;
